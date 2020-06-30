a21198192065b0a6cc5ea9cfe0b1893889643ccc
Ryan Zigler <ryan@MacBook-Pro.local>
2020-06-30 17:50:57 -0500

    Completed fixing tests on part2.js

--- spec/part2.js
+++ spec/part2.js
@@ -56,9 +56,12 @@
       });
 
       it('should return false given an array and a value not in that array', function() {
-        // Replace this line with an `expect` statement that tests
+        // Replace this line with an `expect` statement that tests  ************
+        var array = [1, 2, 3];
+        var value = 4;
+        expect(_.contains(array, value)).to.be.false;
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        //throw new Error('This test is missing.');
       });
 
       it('should return true given a object and a value from that object', function() {
@@ -89,9 +92,10 @@
       });
 
       it('fails for a collection of all-falsy values', function() {
-        // Replace this line with an `expect` statement that tests
+        // Replace this line with an `expect` statement that tests ****************
+        expect(_.every([null, undefined, 0])).to.be.false;
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        //throw new Error('This test is missing.');
       });
 
       it('fails for a collection containing mixed falsy and truthy values', function() {
@@ -147,9 +151,10 @@
       });
 
       it('should fail for a set containing no matching values', function() {
-        // Replace this line with an `expect` statement that tests
+        // Replace this line with an `expect` statement that tests ***********************
+        expect(_.some([1, 3, 5], isEven)).to.equal(false);
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        //throw new Error('This test is missing.');
       });
 
       it('should pass for a collection containing one matching value', function() {
@@ -187,9 +192,14 @@
       });
 
       it('should override properties found on the destination', function() {
-        // Replace this line with an `expect` statement that tests
+        // Replace this line with an `expect` statement that tests ********************
+        var destination = { x: 'x' };
+        var source = { x: 'b' };
+        var extended = _.extend(destination, source);
+
+        expect(extended.x).to.equal('b');
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        //throw new Error('This test is missing.');
       });
 
       it('should not override properties not found in the source', function() {
@@ -215,7 +225,7 @@
 
     describe('defaults', function() {
 
-      it('should return the original target object', function() {
+      it('should return the original target object', function() { //
         /*
          * Our defaults function should only modify the contents of the original object,
          * it should not create a new object with all the same properties
@@ -265,9 +275,15 @@
       });
 
       it('should copy any property whose key is not already set on the target', function() {
-        // Replace this line with an `expect` statement that tests
+        // Replace this line with an `expect` statement that tests ******************
+        var destination = { a: 10 };
+        var source = { b: 1 };
+
+        _.defaults(destination, source);
+
+        expect(destination.b).to.equal(1);
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        // throw new Error('This test is missing.');
       });
 
       it('should not copy a property if that key is already set on the target', function() {
@@ -413,7 +429,7 @@
         memoSpy(10);
         expect(spy).to.have.been.calledOnce;
       });
-      
+
       it('should not run the memoized function twice when given a reference type as an argument', function() {
         // Be careful how you are checking if a set of arguments has been passed in already
         var spy = sinon.spy(function() { return 'Dummy output'; });
@@ -473,7 +489,7 @@
         expect(shuffled).to.not.equal(numbers);
         expect(numbers).to.eql([4, 5, 6]);
       });
-            
+
       it('should maintain same array length', function() {
         var numbers = [1, 1, 2, 3];
         var shuffled = _.shuffle(numbers);
